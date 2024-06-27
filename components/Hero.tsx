5d278aed08faefaed1a5dd50e5a83e5642699560
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-28 01:40:58 +0200

    Added Aceternity UI Spotlight component + Tailwind configurations

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
-  return (
-    <div className="pb-20 pt-20">
-        <h1 className="text-center">Toby Van Buynder</h1>
-    </div>
-  )
+    return (
+        <div className="pb-20 pt-36">
+            <Spotlight className="-top-40 -left-10 md:-left:32 md:-top-20 h-screen" />
+            <Spotlight className="top-10 left-full h-[80vh] w-[50vw}" fill="purple" />
+            <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+            <h1 className="text-center">Toby Van Buynder</h1>
+        </div>
+    )
 }
 
 export default Hero
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,54 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         black: {
           DEFAULT: '#000',
-          100: '#000369'
+          100: '#000319'
         }
-      }
-    }
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
