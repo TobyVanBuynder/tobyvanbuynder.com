c063a6d8a0cd71904c02b71ad9bf6155701daa7d
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-28 01:16:48 +0200

    Moved heading into Hero component + apply new black color

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-20">
+        <h1 className="text-center">Toby Van Buynder</h1>
+    </div>
+  )
+}
+
+export default Hero
