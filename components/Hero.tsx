77aef5dcd4ba472625bbffc37194519a0bc5964a
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-28 02:36:10 +0200

    Added TextGenerateEffect to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
     return (
@@ -13,8 +14,17 @@ const Hero = () => {
             </div>
 
             <div className="flex justify-center relative my-20 z-10">
-                <div className="max-w-[89vw] md:max-w-2xl max-w-[60vw] flex flex-col items-center justify-center">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Toby Van Buynder</h2>
+
+                    <TextGenerateEffect
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                        words="Creative Developer / Game Programmer"
+                    />
+
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi, I&apos;m Toby
+                    </p>
                 </div>
             </div>
         </div>
