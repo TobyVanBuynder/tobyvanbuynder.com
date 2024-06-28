83f5f233f62d6efa2e3cc6054fa10d76d8ffff05
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-28 03:05:26 +0200

    Tweaked grid + added MagicButton + tweaked Hero
    Added react-element-to-jsx-string + react-icons packages

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { MagicButton } from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
     return (
-        <div className="pb-20 pt-36">
+        <div className="pb-8 pt-24">
             <Spotlight className="-top-40 -left-10 md:-left:32 md:-top-20 h-screen" />
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw}" fill="purple" />
             <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
@@ -23,8 +25,12 @@ const Hero = () => {
                     />
 
                     <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                        Hi, I&apos;m Toby
+                        based in Copenhagen, Denmark
                     </p>
+
+                    <a href="#about">
+                        <MagicButton title='Show My Work' icon={<FaLocationArrow />} position={'right'} />
+                    </a>
                 </div>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,16 +30,25 @@ const config: Config = {
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
       },
       keyframes: {
         spotlight: {
           "0%": {
-            opacity: 0,
+            opacity: "0",
             transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
-            opacity: 1,
+            opacity: "1",
             transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
           },
         },
       },
@@ -53,7 +62,7 @@ const config: Config = {
         {
           "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="100" height="100" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
           "bg-grid-small": (value: any) => ({
