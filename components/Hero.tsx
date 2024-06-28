898723cd7b89ddaa23a7eea85ee8d879f0018869
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-06-28 02:15:21 +0200

    Added default dark mode + styled grid for Hero component
    Added mini-svg-data-uri + next-themes packages

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,16 @@ const Hero = () => {
             <Spotlight className="-top-40 -left-10 md:-left:32 md:-top-20 h-screen" />
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw}" fill="purple" />
             <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-            <h1 className="text-center">Toby Van Buynder</h1>
+
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.03] flex items-center justify-center absolute top-0 left-0">
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+            </div>
+
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl max-w-[60vw] flex flex-col items-center justify-center">
+                    <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Toby Van Buynder</h2>
+                </div>
+            </div>
         </div>
     )
 }
